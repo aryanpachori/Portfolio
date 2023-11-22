e9fed19b8b5968be2b1e58ea957ea52a80c48e04
Aryan <aryanpachori03@gmail.com>
2023-11-23 00:03:24 +0530

    Added Ref (Header)

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,14 +1,21 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useInView } from "react-intersection-observer";
+import { useSectionInView } from "@/lib/hooks";
 export default function Intro() {
+
+ const {ref } = useSectionInView("Home", 0.5);
+
   return (
-    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+    <section id="home" ref={ref}
+    className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
